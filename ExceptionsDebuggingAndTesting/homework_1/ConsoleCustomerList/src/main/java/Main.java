2f46e6058ee1471ae0e2d6f73c59e974640e87cb
melivan73 <melivan73@gmail.com>
2024-12-21 17:50:35 +0300

    Module 18 homework commit

--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
@@ -1,13 +1,16 @@
+import org.apache.logging.log4j.*;
+
 import java.util.Scanner;
 
 public class Main {
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
     private static final String ADD_COMMAND = "add Василий Петров " +
             "vasily.petrov@gmail.com +79215637722";
     private static final String COMMAND_EXAMPLES = "\t" + ADD_COMMAND + "\n" +
             "\tlist\n\tcount\n\tremove Василий Петров";
     private static final String COMMAND_ERROR = "Wrong command! Available command examples: \n" +
             COMMAND_EXAMPLES;
-    private static final String helpText = "Command examples:\n" + COMMAND_EXAMPLES;
+    private static final String HELPTEXT = "Command examples:\n" + COMMAND_EXAMPLES;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -16,17 +19,30 @@ public class Main {
         while (true) {
             String command = scanner.nextLine();
             String[] tokens = command.split("\\s+", 2);
+            LOGGER.info(command);
 
             if (tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
+                try {
+                    executor.addCustomer(tokens[1]);
+                } catch (CommandParamsCountException | WrongEmailFormatException | WrongPhoneFormatException e ) {
+                    LOGGER.error(e.getMessage());
+                    System.out.println(e.getMessage());
+                }
             } else if (tokens[0].equals("list")) {
                 executor.listCustomers();
             } else if (tokens[0].equals("remove")) {
-                executor.removeCustomer(tokens[1]);
+                try {
+                    executor.removeCustomer(tokens[1]);
+                } catch (CommandParamsCountException e) {
+                    LOGGER.error(e.getMessage());
+                    System.out.println(e.getMessage());
+                }
             } else if (tokens[0].equals("count")) {
                 System.out.println("There are " + executor.getCount() + " customers");
             } else if (tokens[0].equals("help")) {
-                System.out.println(helpText);
+                System.out.println(HELPTEXT);
+            }else if (tokens[0].equals("quit")) {
+                break;
             } else {
                 System.out.println(COMMAND_ERROR);
             }
